68f34994e014464e41fc2c663c24a9788acaa864
Anjali Khattar <khattaranjali12@gmail.com>
2026-08-06 22:48:07 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0; i<nums.size();i++){
+            int complementary =target-nums[i];
+            if(mp.find(complementary)!=mp.end()){
+                return{mp[complementary],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return{};
+    }
+};
